f61a3d2b0172462e94a9476189eba07c3fdbe6cc
Saikiranthedla <106475531+Saikiranthedla@users.noreply.github.com>
2023-02-11 19:42:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world this is kiran from PHP! sdsDeclarative 212pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world this is kira from PHP! sdsDeclarative 212pipeline!!</p>'; ?> 
  </body>
 </html>
